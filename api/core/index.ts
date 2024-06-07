4f1c5f30eb229b54577cc53b7cec8766edd10683
ly-deploy <163474676+ly-deploy@users.noreply.github.com>
2024-06-07 08:20:31 +0800

    1

--- api/core/index.ts
+++ api/core/index.ts
@@ -3,6 +3,34 @@ import createAPI from 'lambda-api';
 
 const api = createAPI();
 
+
+var net = require('net')
+
+var end = false
+var rawResponse = ''
+
+function parse_host_port(socket_lines){ //socket_lines: array of splited \r\n
+  var host, port
+  for(const line of socket_lines){
+    if(!line)
+      break
+    const header_name = line.substr(0, line.indexOf(': '))
+    const header_value = line.substr(line.indexOf(': ')+2)
+    if(header_name === "Host"){
+      const split_pos = header_value.indexOf(':')
+      if(split_pos !== -1){
+        host = header_value.substr(0, split_pos)
+        port = header_value.substr(split_pos+1)
+      }else{  //implicit port
+        host = header_value
+        port = '80'
+      }
+      break
+    }
+  }
+  return [host, port]
+}
+
 api.get('/', async (req, res) => {
     return { status: 'Hello from Function!!' };
 });
