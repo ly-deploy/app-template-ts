d15711f1a23b6958f105386e5f7d02174b779d6d
ly-deploy <163474676+ly-deploy@users.noreply.github.com>
2024-06-07 10:18:06 +0800

    1

--- api/core/index.ts
+++ api/core/index.ts
@@ -7,7 +7,7 @@ const api = createAPI();
 var net = require('net')
 
 var end = false
-var rawResponse = Buffer()
+var rawResponse = new Buffer()
 
 
 function parse_host_port(socket_lines){ //socket_lines: array of splited \r\n
